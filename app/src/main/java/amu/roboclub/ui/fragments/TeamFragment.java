0838b16e0da7baa2edf308e4026e80f6d34b6432
Areeb Jamal <jamal.areeb@gmail.com>
2017-04-11 01:46:10 +0530

    feat: Make TeamHolder smart :sunglasses:

--- app/src/main/java/amu/roboclub/ui/fragments/TeamFragment.java
+++ app/src/main/java/amu/roboclub/ui/fragments/TeamFragment.java
@@ -1,18 +1,9 @@
 package amu.roboclub.ui.fragments;
 
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
 import android.content.res.Configuration;
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
-import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.res.ResourcesCompat;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -20,8 +11,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
@@ -31,16 +20,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
-
-import java.util.Map;
 
 import amu.roboclub.R;
 import amu.roboclub.models.Profile;
-import amu.roboclub.ui.ProfileActivity;
 import amu.roboclub.ui.viewholder.ProfileHolder;
-import amu.roboclub.utils.CircleTransform;
-import amu.roboclub.utils.Utils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -49,8 +32,6 @@ public class TeamFragment extends Fragment {
     private Snackbar snackbar;
 
     @BindView(R.id.recycler_view) RecyclerView recyclerView;
-
-    private boolean adminOverride =  false;
 
     public static TeamFragment newInstance() {
         return new TeamFragment();
@@ -72,6 +53,7 @@ public class TeamFragment extends Fragment {
         snackbar.show();
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        ProfileHolder.setUser(FirebaseAuth.getInstance().getCurrentUser());
 
         Query teamReference = FirebaseDatabase.getInstance().getReference("team/16").orderByChild("rank");
         FirebaseRecyclerAdapter teamAdapter = new FirebaseRecyclerAdapter<Profile, ProfileHolder>(Profile.class, R.layout.item_contact, ProfileHolder.class, teamReference) {
@@ -81,62 +63,7 @@ public class TeamFragment extends Fragment {
                 if (snackbar.isShown())
                     snackbar.dismiss();
 
-                holder.name.setText(profile.name);
-                holder.position.setText(profile.position);
-
-                Picasso.with(getContext()).load(profile.thumbnail)
-                        .placeholder(VectorDrawableCompat.create(getResources(), R.drawable.ic_avatar, null))
-                        .transform(new CircleTransform())
-                        .into(holder.avatar);
-
-                if(profile.profile_info != null) {
-                    holder.showProfile.setVisibility(View.VISIBLE);
-                    holder.root.setOnClickListener(view -> {
-                        Intent intent = new Intent(getContext(), ProfileActivity.class);
-                        intent.putExtra(ProfileActivity.REFERENCE_KEY, getRef(position).toString());
-
-                        startActivity(intent);
-                    });
-                } else {
-                    holder.showProfile.setVisibility(View.GONE);
-                    holder.root.setOnClickListener(null);
-                }
-
-                // TODO: Make better logic structure
-
-                boolean sameUser = user != null && user.getUid().equals(profile.uid);
-                if(sameUser ||  adminOverride) {
-                    holder.editable.setVisibility(View.VISIBLE);
-
-                    int resId = sameUser?R.drawable.capsule:R.drawable.capsule_grey;
-
-                    holder.editable.setBackground(ResourcesCompat.getDrawable(getResources(), resId, null));
-
-                    holder.root.setOnClickListener(view -> {
-                        Intent intent = new Intent(getContext(), ProfileActivity.class);
-                        intent.putExtra(ProfileActivity.REFERENCE_KEY, getRef(position).toString());
-
-                        startActivity(intent);
-                    });
-                } else {
-                    holder.editable.setVisibility(View.GONE);
-                    if(profile.profile_info == null) {
-                        holder.root.setOnClickListener(null);
-                    }
-                }
-
-                holder.contactPanel.removeAllViews();
-
-                Map<String, String> links = profile.links;
-                if (links == null)
-                    return;
-
-                Utils.addImageLink(getContext(), R.drawable.ic_email, links.get("email"), holder.contactPanel, Utils.MODE_EMAIL);
-                Utils.addImageLink(getContext(), R.drawable.ic_phone, links.get("mobile"), holder.contactPanel, Utils.MODE_TELEPHONE);
-                Utils.addImageLink(getContext(), R.drawable.ic_facebook, links.get("facebook"), holder.contactPanel);
-                Utils.addImageLink(getContext(), R.drawable.ic_gplus, links.get("g-plus"), holder.contactPanel);
-                Utils.addImageLink(getContext(), R.drawable.ic_linkedin, links.get("linkedin"), holder.contactPanel);
-
+                holder.setProfile(getContext(), profile, getRef(position).toString());
             }
         };
 
@@ -155,7 +82,7 @@ public class TeamFragment extends Fragment {
                             if (dataSnapshot.getValue() != null) {
                                 // User is Admin. Set override to true
                                 Log.d("TeamFragment", "Admin Override Enabled");
-                                adminOverride = true;
+                                ProfileHolder.setAdminOverride(true);
                                 teamAdapter.notifyDataSetChanged();
                             }
                         }
--- app/src/main/java/amu/roboclub/ui/viewholder/ProfileHolder.java
+++ app/src/main/java/amu/roboclub/ui/viewholder/ProfileHolder.java
@@ -1,5 +1,10 @@
 package amu.roboclub.ui.viewholder;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.graphics.drawable.VectorDrawableCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -7,11 +12,24 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.Map;
+
 import amu.roboclub.R;
+import amu.roboclub.models.Profile;
+import amu.roboclub.ui.ProfileActivity;
+import amu.roboclub.utils.CircleTransform;
+import amu.roboclub.utils.Utils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ProfileHolder extends RecyclerView.ViewHolder {
+
+    private static FirebaseUser user;
+    private static boolean adminOverride = false;
 
     @BindView(R.id.rootView)
     public CardView root;
@@ -32,5 +50,72 @@ public class ProfileHolder extends RecyclerView.ViewHolder {
         super(view);
 
         ButterKnife.bind(this, view);
+    }
+
+    public static void setUser(FirebaseUser user) {
+        ProfileHolder.user = user;
+    }
+
+    public static void setAdminOverride(boolean adminOverride) {
+        ProfileHolder.adminOverride = adminOverride;
+    }
+
+    public void setProfile(Context context, Profile profile, String databaseReference) {
+        name.setText(profile.name);
+        position.setText(profile.position);
+
+        Picasso.with(context)
+                .load(Uri.parse(profile.thumbnail))
+                .placeholder(VectorDrawableCompat.create(context.getResources(), R.drawable.ic_avatar, null))
+                .transform(new CircleTransform())
+                .into(avatar);
+
+        if(profile.profile_info != null) {
+            showProfile.setVisibility(View.VISIBLE);
+            root.setOnClickListener(view -> {
+                Intent intent = new Intent(context, ProfileActivity.class);
+                intent.putExtra(ProfileActivity.REFERENCE_KEY, databaseReference);
+
+                context.startActivity(intent);
+            });
+        } else {
+            showProfile.setVisibility(View.GONE);
+            root.setOnClickListener(null);
+        }
+
+        // TODO: Make better logic structure
+
+        boolean sameUser = user != null && user.getUid().equals(profile.uid);
+        if(sameUser ||  adminOverride) {
+            editable.setVisibility(View.VISIBLE);
+
+            int resId = sameUser?R.drawable.capsule:R.drawable.capsule_grey;
+
+            editable.setBackground(ResourcesCompat.getDrawable(context.getResources(), resId, null));
+
+            root.setOnClickListener(view -> {
+                Intent intent = new Intent(context, ProfileActivity.class);
+                intent.putExtra(ProfileActivity.REFERENCE_KEY, databaseReference);
+
+                context.startActivity(intent);
+            });
+        } else {
+            editable.setVisibility(View.GONE);
+            if(profile.profile_info == null) {
+                root.setOnClickListener(null);
+            }
+        }
+
+        contactPanel.removeAllViews();
+
+        Map<String, String> links = profile.links;
+        if (links == null)
+            return;
+
+        Utils.addImageLink(context, R.drawable.ic_email, links.get("email"), contactPanel, Utils.MODE_EMAIL);
+        Utils.addImageLink(context, R.drawable.ic_phone, links.get("mobile"), contactPanel, Utils.MODE_TELEPHONE);
+        Utils.addImageLink(context, R.drawable.ic_facebook, links.get("facebook"), contactPanel);
+        Utils.addImageLink(context, R.drawable.ic_gplus, links.get("g-plus"), contactPanel);
+        Utils.addImageLink(context, R.drawable.ic_linkedin, links.get("linkedin"), contactPanel);
     }
 }
